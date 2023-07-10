85e8c96e33b88dcf06700941e1a8d4b45e72a890
tshumay <trent@iotdesignshop.com>
2023-07-10 13:23:55 -0700

    Working BLE comms between the devices

--- Python/dexhand-ble.py
+++ Python/dexhand-ble.py
@@ -12,10 +12,18 @@ from bleak import BleakScanner
 import mediapipe as mp
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
-#from mediapipe.tasks import python
-#from mediapipe.tasks.python import vision
+
+from itertools import count, takewhile
+from typing import Iterator
 
 import numpy as np
+
+from bleak import BleakClient, BleakScanner
+from bleak.backends.characteristic import BleakGATTCharacteristic
+from bleak.backends.device import BLEDevice
+from bleak.backends.scanner import AdvertisementData
+
+
 
 MARGIN = 10  # pixels
 FONT_SIZE = 1
@@ -101,30 +109,73 @@ async def hand_tracking():
             cap.release()
             cv2.destroyAllWindows()
 
-                    
-            
-        
+
+
+UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+       
 async def ble_communication():
-        stop_event = asyncio.Event()
+    """This is a simple "terminal" program that uses the Nordic Semiconductor
+    (nRF) UART service. It reads from stdin and sends each line of data to the
+    remote device. Any data received from the device is printed to stdout.
+    """
 
-        def callback(device, advertisement_data):
-            if (advertisement_data.local_name and advertisement_data.local_name.startswith("DexHand")):
-                #print(f"Device {device.address} (name: {device.name}) found.")
-                #print(f"Advertised data: {advertisement_data}")
-                pass
+    def match_nus_uuid(device: BLEDevice, adv: AdvertisementData):
+        # This assumes that the device includes the UART service UUID in the
+        # advertising data. This test may need to be adjusted depending on the
+        # actual advertising data supplied by the device.
+        if UART_SERVICE_UUID.lower() in adv.service_uuids:
+            return True
 
-        async with BleakScanner(callback) as scanner:
-            try:
-                await(stop_event.wait())
-                print('Scanning complete')
-            except asyncio.CancelledError:
-                print('BLE communication task has been cancelled.')
-                #await scanner.stop()
-                
+        return False
 
-        
-        # Wait for 10 seconds
-        asyncio.run(asyncio.sleep(10.0))
+    device = await BleakScanner.find_device_by_filter(match_nus_uuid)
+
+    if device is None:
+        print("No DexHand device was found to establish a connection.")
+        sys.exit(1)
+
+    def handle_disconnect(_: BleakClient):
+        print("Device was disconnected, goodbye.")
+        # cancelling all tasks effectively ends the program
+        for task in asyncio.all_tasks():
+            task.cancel()
+
+    def handle_rx(_: BleakGATTCharacteristic, data: bytearray):
+        # Convert received byte array to string
+        data = data.decode("utf-8")
+
+        # Responses are pretty basic - this can be extended later if more
+        # data comes back from the hand
+        if (data.startswith("HB:")):
+            print("Heartbeat received: ", data[3:])
+        else:
+            print("Unknown message received:", data)
+
+    async with BleakClient(device, disconnected_callback=handle_disconnect) as client:
+        await client.start_notify(UART_TX_CHAR_UUID, handle_rx)
+
+        print("Connected, start typing and press ENTER...")
+
+        loop = asyncio.get_running_loop()
+        nus = client.services.get_service(UART_SERVICE_UUID)
+        rx_char = nus.get_characteristic(UART_RX_CHAR_UUID)
+
+        while True:
+            # This waits until you type a line and press ENTER.
+            # A real terminal program might put stdin in raw mode so that things
+            # like CTRL+C get passed to the remote device.
+            data = await loop.run_in_executor(None, sys.stdin.buffer.readline)
+
+            # data will be empty on EOF (e.g. CTRL+D on *nix)
+            if not data:
+                break
+
+            # Send the data via BLE
+            await client.write_gatt_char(rx_char, data, True)
+            print("Sent:", data)
 
 
 async def main():
